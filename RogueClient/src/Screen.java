06964abc0d74fcb8675f120303516d10a22c87fa
Nyxaeroz <thijsvanloenhout@live.nl>
2018-05-31 14:51:02 +0200

    Fix printArray method and start getWidth and getHeight methods

--- RogueClient/src/Screen.java
+++ RogueClient/src/Screen.java
@@ -1,47 +1,58 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
 
-public class Screen extends JTextPane{
+public class Screen extends JTextPane {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private int fontWidth = -1;
-	private Style style; 
+	private int fontHeight = -1;
+	private Style style;
 	private Font font = new Font("monospaced", Font.PLAIN, 12);
-	
+
 	public Screen() {
 		setForeground(Color.WHITE);
 		setBackground(Color.BLACK);
 		addStyle("style", null);
-//		setFont(new Font("monospaced", Font.PLAIN, 12));
+		// setFont(new Font("monospaced", Font.PLAIN, 12));
 		setFont(font);
 		System.out.println(getFontWidth());
-		
-		
-	}
-	
-	public void printArray(String[][] world) {
-		StringBuilder output = new StringBuilder();
-		for (int i = 0; i < world.length; i++) {
-			for (int j = 0; j < world[i].length; j++) {
-				if (world[i][j] == null) {
-					world[i][j]= " ";
-				}
-				output.append(world[i][j]);
-			}
-			output.append("\n");
-		}
-		setText(output.toString());
-		
+
 	}
 
+	public void printArray(String[][] world) {
+		clearScreen();
+		StyledDocument doc = getStyledDocument();
+
+		for (int j = world.length; j >= 0; j--) {
+
+			try {
+				doc.insertString(0, "\n", null);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+
+			for (int i = world.length; i >= 0; i--) {
+				try {
+					doc.insertString(0, Integer.toString(i), null);
+				} catch (BadLocationException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public void clearScreen() { // ook nog style clearen?
+		setText("");
+	}
 
 	public int getFontWidth() {
 		if (fontWidth == -1) {
@@ -50,11 +61,29 @@ public class Screen extends JTextPane{
 			fontWidth = fontMetrics.charWidth('l');
 		}
 		return fontWidth;
+
+	}
+
+	public int getFontHeigt() {
+		if (fontHeight == -1) {
+			BufferedImage bf = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+			FontMetrics fontMetrics = bf.createGraphics().getFontMetrics(font);
+			fontHeight = fontMetrics.charWidth('l');
+		}
+		return fontHeight;
+
+	}
+	
+	public int getWidth() {
+		return 0;
 		
 	}
 	
-	public void setupenzo(String[][] world) {
-		printArray(world);
+	public int getHeight() {
+		return 0;
+	}
+
+	public void init(String[][] world) {
 		if (getWidth() != 0) {
 			for (int i = 0; i < world.length; i++) {
 				for (int j = 0; j < world[i].length; j++) {
@@ -63,8 +92,8 @@ public class Screen extends JTextPane{
 			}
 
 		}
+		printArray(world);
 		System.out.println(getWidth() + "   " + getHeight());
 	}
-	
-	
+
 }
